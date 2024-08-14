237f8f84618b43936c95d6370fb29160e145b021
zzergAtStage <hightemplar98@iclod.com>
2024-08-14 22:38:25 +0200

    dev: scroll to top updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CvQueriedComponent } from "./pages/cv-queried/cv-queried.component";
 import { CvStaticComponent } from "./pages/cv-static/cv-static.component";
 import { MatrixCodeComponent } from "./pages/matrix-code/matrix-code.component";
 import { LanguageSelectorComponent } from "./pages/navigation/language-selector/language-selector.component";
+import { ScrollToTopComponent } from './pages/navigation/scroll-to-top/scroll-to-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CvQueriedComponent, CvStaticComponent, RouterModule, MatrixCodeComponent, LanguageSelectorComponent],
+  imports: [RouterOutlet, CvQueriedComponent, CvStaticComponent, RouterModule, MatrixCodeComponent, LanguageSelectorComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
