0f30c690f78e4b8fca2fb942f646a9b0ec2a7e49
zzergAtStage <hightemplar98@iclod.com>
2024-08-14 15:29:12 +0200

    build scripts added

---
+++ scripts/build_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ng build --configuration=production --base-href=https://zzergatstage.github.io/cv-sample/
+rm -rf ../docs
+cp -r dist/cv-sample/browser ../docs
+cp ../docs/index.html ../docs/404.html
