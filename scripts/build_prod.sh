93dcd1d06705347063ef7d41470c6d10eabb221d
zzergAtStage <hightemplar98@iclod.com>
2024-08-14 16:03:12 +0200

    rebuild

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -2,5 +2,5 @@
 
 ng build --configuration=production --base-href=https://zzergatstage.github.io/cv-sample/
 rm -rf ../docs
-cp -r dist/cv-sample/browser ../docs
+cp -r ../dist/cv-sample/browser ../docs
 cp ../docs/index.html ../docs/404.html
